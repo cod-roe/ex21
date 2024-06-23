efa81e5756a9dc63809c5c60e1dd4bf4ed426586
cod-roe <paradox573@gmail.com>
2024-06-23 15:06:47 +0900

    改良

--- python/src/sample1.py
+++ python/src/sample1.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import lightgbm as lgb
 
 # x軸:時刻
 x = np.arange(0, 100, 0.5)
@@ -11,4 +12,9 @@ y = np.sin(2.0 * np.pi * (x * Hz) / 100)
 # グラフを描画
 plt.plot(x, y)
 plt.savefig('test.png')
+
+#%%
+print('Yaah!')
+
+
 
